3ef4c8285db4cbee378fa5d32705ffd52e5b9459
Max Katkalov <makskatkalov@gmail.com>
2024-05-25 18:02:44 +0300

    fix:docs: Implement documentation and update README.md

--- employee_lunch_api/urls.py
+++ employee_lunch_api/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 
 urlpatterns = [
@@ -12,5 +17,20 @@ urlpatterns = [
         include("restaurant_service.urls", namespace="restaurant"),
     ),
     path("api/polling/", include("polling_service.urls", namespace="polling")),
-    path("__debug__/", include("debug_toolbar.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc"
+    ),
+]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
